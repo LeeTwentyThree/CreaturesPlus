268feb8a66376ed3ff4e4d10053a4262754ac821
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2020-12-20 13:38:23 -0500

    Setting up the mod

---
+++ CreaturesPlus/CreaturesPlus/Mod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ECCLibrary;
+using MoreCreatures;
+using QModManager.API;
+using QModManager.API.ModLoading;
+using UnityEngine;
+
+namespace CreaturesPlus
+{
+    [QModCore]
+    public static class QPatch
+    {
+        public static AssetBundle assetBundle;
+        public static GargantuanLeviathanPrefab gargantuan;
+
+        [QModPatch]
+        public static void Patch()
+        {
+            assetBundle = ECCHelpers.LoadAssetBundleFromAssetsFolder(Assembly.GetExecutingAssembly(), "creaturesplusassets");
+            ECCAudio.RegisterClips(assetBundle);
+
+            gargantuan = new GargantuanLeviathanPrefab("GargantuanLeviathan", "Gargantuan Leviathan", "An ancient predator thought to be extinct", assetBundle.LoadAsset<GameObject>("GargantuanPrefab"), null);
+            gargantuan.Patch();
+        }
+    }
+}
